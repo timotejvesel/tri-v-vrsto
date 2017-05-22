5cb1deb47995b1d549b717c9402e1fa651005ba5
timotejvesel <timotej.vesel@gmail.com>
2017-05-22 19:17:36 +0200

    Add files via upload

--- tri-v-vrsto.py
+++ tri-v-vrsto.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import model
+import tkinter.messagebox
 
 VELIKOST_TABLICE = 100
 ODMIK = 10
@@ -17,49 +18,75 @@ class Gui:
 			height=3 * VELIKOST_TABLICE + ODMIK
 		)
 		self.igralna_plosca.pack()
-        
-		self.klik()
 		self.osvezi_prikaz()
+		self.klik()
+		
 		
 
-	def klik(self):
-		self.igralna_plosca.tag_bind("kvadrat0",'<Button-1>', self.test)
-		self.igralna_plosca.tag_bind("kvadrat1",'<Button-1>', self.test)
 
+	def kvadrat(self, event, n):
+		# Matrične koordinate
+		x = n % 3
+		y = n // 3
 
+		if self.igra.clovek(n) == False:
+			tk.messagebox.showinfo(" ", "To polje je že zasedeno!")
+		else:
+			self.igra.clovek(n)
 
-	def test(self, event):
-		self.igralna_plosca.create_rectangle(50, 25, 150, 75, fill="blue")
-		self.igra.clovek(0)
+		self.igralna_plosca.create_line(
+			2 * ODMIK + x * VELIKOST_TABLICE,
+			2 * ODMIK + y * VELIKOST_TABLICE,
+			VELIKOST_TABLICE - ODMIK + x * VELIKOST_TABLICE, 
+			VELIKOST_TABLICE - ODMIK + y * VELIKOST_TABLICE,
+			width=3
+		)
+
+		self.igralna_plosca.create_line(
+			2* ODMIK + x * VELIKOST_TABLICE,
+			VELIKOST_TABLICE - ODMIK + y * VELIKOST_TABLICE,
+			VELIKOST_TABLICE - ODMIK + x * VELIKOST_TABLICE,
+			2 * ODMIK + y * VELIKOST_TABLICE,
+			width=3
+		)
+
+		self.igra.racunalnik()
+		
 		print(self.igra)
 
+		if self.igra.zmagovalne_kombinacije_clovek() == True:
+			tk.messagebox.showinfo(" ", "Zmagal si!")
+			self.odstrani_klik()
 
 
-	
+
+			
+
 	def osvezi_prikaz(self):
 		self.igralna_plosca.delete('all')
 
+	
 		# Tablica, prva vrsta
 		self.igralna_plosca.create_rectangle(
 			ODMIK,
 			ODMIK, 
 			VELIKOST_TABLICE,
 			VELIKOST_TABLICE, 
-			fill="FloralWhite", tags="kvadrat0"
+			fill="FloralWhite", tags="kvadrat0_tag"
 		)
 		self.igralna_plosca.create_rectangle(
 			VELIKOST_TABLICE + ODMIK,
 			ODMIK, 
 			2 * VELIKOST_TABLICE,
 			VELIKOST_TABLICE,
-			fill="FloralWhite", tags="kvadrat1"
+			fill="FloralWhite", tags="kvadrat1_tag"
 		)
 		self.igralna_plosca.create_rectangle(
 			2 * VELIKOST_TABLICE + ODMIK,
 			ODMIK,
 			3 * VELIKOST_TABLICE,
 			VELIKOST_TABLICE,
-		 	fill="FloralWhite", tags="kvadrat2"
+		 	fill="FloralWhite", tags="kvadrat2_tag"
 		)
 
 		# Tablica, druga vrsta
@@ -68,21 +95,21 @@ class Gui:
 			2 * VELIKOST_TABLICE,
 			VELIKOST_TABLICE,
 			VELIKOST_TABLICE + ODMIK,
-		 	fill="FloralWhite", tags="kvadrat3"
+		 	fill="FloralWhite", tags="kvadrat3_tag"
 		)
 		self.igralna_plosca.create_rectangle(
 			VELIKOST_TABLICE + ODMIK,
 			2 * VELIKOST_TABLICE,
 			2 * VELIKOST_TABLICE,
 			VELIKOST_TABLICE + ODMIK,
-			fill="FloralWhite", tags="kvadrat4"
+			fill="FloralWhite", tags="kvadrat4_tag"
 		)
 		self.igralna_plosca.create_rectangle(
 			2 * VELIKOST_TABLICE + ODMIK,
 			2 * VELIKOST_TABLICE,
 			3* VELIKOST_TABLICE,
 			VELIKOST_TABLICE + ODMIK,
-			fill="FloralWhite", tags="kvadrat5"
+			fill="FloralWhite", tags="kvadrat5_tag"
 		)
 
 		# Tablica, tretja vrsta
@@ -91,36 +118,52 @@ class Gui:
 			3 * VELIKOST_TABLICE,
 			VELIKOST_TABLICE,
 			2 * VELIKOST_TABLICE + ODMIK,
-			fill="FloralWhite", tags="kvadrat6"
+			fill="FloralWhite", tags="kvadrat6_tag"
 		)
 		self.igralna_plosca.create_rectangle(
 			VELIKOST_TABLICE + ODMIK,
 			3 * VELIKOST_TABLICE,
 			2 * VELIKOST_TABLICE,
 			2 * VELIKOST_TABLICE + ODMIK,
-			fill="FloralWhite", tags="kvadrat7"
+			fill="FloralWhite", tags="kvadrat7_tag"
 		)
 		self.igralna_plosca.create_rectangle(
 			2 * VELIKOST_TABLICE + ODMIK,
 			3 * VELIKOST_TABLICE,
 			3 * VELIKOST_TABLICE,
 			2 * VELIKOST_TABLICE + ODMIK,
-			fill="FloralWhite", tags="kvadrat8"
+			fill="FloralWhite", tags="kvadrat8_tag"
 		)
 
+	def klik(self):
+		self.igralna_plosca.tag_bind("kvadrat0_tag","<Button-1>", lambda event: self.kvadrat(event, 0))
+		self.igralna_plosca.tag_bind("kvadrat1_tag","<Button-1>", lambda event: self.kvadrat(event, 1))
+		self.igralna_plosca.tag_bind("kvadrat2_tag","<Button-1>", lambda event: self.kvadrat(event, 2))
+		self.igralna_plosca.tag_bind("kvadrat3_tag","<Button-1>", lambda event: self.kvadrat(event, 3))
+		self.igralna_plosca.tag_bind("kvadrat4_tag","<Button-1>", lambda event: self.kvadrat(event, 4))
+		self.igralna_plosca.tag_bind("kvadrat5_tag","<Button-1>", lambda event: self.kvadrat(event, 5))
+		self.igralna_plosca.tag_bind("kvadrat6_tag","<Button-1>", lambda event: self.kvadrat(event, 6))
+		self.igralna_plosca.tag_bind("kvadrat7_tag","<Button-1>", lambda event: self.kvadrat(event, 7))
+		self.igralna_plosca.tag_bind("kvadrat8_tag","<Button-1>", lambda event: self.kvadrat(event, 8))
+
+
+	def odstrani_klik(self):
+		self.igralna_plosca.tag_unbind("kvadrat0_tag",'<Button-1>')
+		self.igralna_plosca.tag_unbind("kvadrat1_tag",'<Button-1>')
+		self.igralna_plosca.tag_unbind("kvadrat2_tag",'<Button-1>')
+		self.igralna_plosca.tag_unbind("kvadrat3_tag",'<Button-1>')
+		self.igralna_plosca.tag_unbind("kvadrat4_tag",'<Button-1>')
+		self.igralna_plosca.tag_unbind("kvadrat5_tag",'<Button-1>')
+		self.igralna_plosca.tag_unbind("kvadrat6_tag",'<Button-1>')
+		self.igralna_plosca.tag_unbind("kvadrat7_tag",'<Button-1>')
+		self.igralna_plosca.tag_unbind("kvadrat8_tag",'<Button-1>')
 		
 		
 		
 
 		
 		
-		
-
-
-
-
-
-
+	
 okno = tk.Tk()
 moj_program = Gui(okno)
 okno.mainloop()
